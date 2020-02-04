90b74c3d20193e63def40912250c1e9906fea9a1
vitorrgg <vitorrgg@users.noreply.github.com>
2020-02-04 14:34:44 +0000

    Deploying to dist from  - e5ed8651a05626a717bc48ac6401a2f0f0fae7f5 🚀

---
+++ precache-manifest.8af0cc57135cf078ccf56ce498dc8f5b.js
@@ -0,0 +1,209 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65e819dfd36370879b02",
+    "url": "/03754d3c2d870ab39771.css"
+  },
+  {
+    "revision": "eea4b3de23b7df6da71f",
+    "url": "/0802f8548567f096d594.js"
+  },
+  {
+    "revision": "ba97f7d44ea07cba36f675284227eaaa",
+    "url": "/0802f8548567f096d594.js.LICENSE.txt"
+  },
+  {
+    "revision": "03e1db9ba4e1ad5982eb",
+    "url": "/0f3fd460a43bef2656f3.css"
+  },
+  {
+    "revision": "2648a4c33be0bd2185f9",
+    "url": "/13755649ba70a2413223.js"
+  },
+  {
+    "revision": "635186055179faef2465",
+    "url": "/1dc5afdc2e5efe349726.js"
+  },
+  {
+    "revision": "1389e9c183a7c9fe4427",
+    "url": "/28a1f44a040806c6c3c2.js"
+  },
+  {
+    "revision": "7fd23991b0b37b49066e7a34e0535285",
+    "url": "/28a1f44a040806c6c3c2.js.LICENSE.txt"
+  },
+  {
+    "revision": "6afc43e40e553a4101cd",
+    "url": "/40bbbfb32dbcfcbc515a.js"
+  },
+  {
+    "revision": "6082a162be8671dcfe75",
+    "url": "/4409a4e104d93fa96dc3.js"
+  },
+  {
+    "revision": "9e23fde043e6a806be80",
+    "url": "/49dd63ada79bbaeb2c61.js"
+  },
+  {
+    "revision": "71cc31433634651ed8aedf9cb29b5906",
+    "url": "/49dd63ada79bbaeb2c61.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc103dd294edd105b403",
+    "url": "/6520f2cf6c47767cd2ab.js"
+  },
+  {
+    "revision": "16b9e2351602c7e0c346",
+    "url": "/6f71d2fb637564aa3863.js"
+  },
+  {
+    "revision": "65e819dfd36370879b02",
+    "url": "/7b5f93a107c9dd22bcd7.js"
+  },
+  {
+    "revision": "5ba6fbc7516a5c3041f03e009339f3ba",
+    "url": "/7b5f93a107c9dd22bcd7.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fcdd06ac007819038d0",
+    "url": "/7bef469f5b3236e239fe.css"
+  },
+  {
+    "revision": "caa30a3ed7d80bede772",
+    "url": "/7c3e8aec82b3a10263f3.css"
+  },
+  {
+    "revision": "44c7254da28ca22bd127",
+    "url": "/9eaa452d22c845e6137d.js"
+  },
+  {
+    "revision": "444997874bd2cd6b400c",
+    "url": "/ab0bb6cc747fe46ac155.js"
+  },
+  {
+    "revision": "ccda136fe41a917d5bcc",
+    "url": "/admin.5433527572beed59f86a.js"
+  },
+  {
+    "revision": "5dae3c5565db289db1b2a92bb4754b2d",
+    "url": "/admin.5433527572beed59f86a.js.LICENSE.txt"
+  },
+  {
+    "revision": "635186055179faef2465",
+    "url": "/aebc193a594ad2fda9bf.css"
+  },
+  {
+    "revision": "a4b1de84fd48407c66b7",
+    "url": "/b1d9acd17925a0386518.js"
+  },
+  {
+    "revision": "9e23fde043e6a806be80",
+    "url": "/b520635dc4b83caa5395.css"
+  },
+  {
+    "revision": "eea4b3de23b7df6da71f",
+    "url": "/b68eab0426da7d99f21c.css"
+  },
+  {
+    "revision": "8fcdd06ac007819038d0",
+    "url": "/c500c82133caac4641c8.js"
+  },
+  {
+    "revision": "1389e9c183a7c9fe4427",
+    "url": "/ce1ba2f9f4826a2d02b6.css"
+  },
+  {
+    "revision": "087528076daec42af6e8",
+    "url": "/checkout.91eddf5b579b45a1d1cc.js"
+  },
+  {
+    "revision": "07c7b1e085ef93ca096422e7422c8d96",
+    "url": "/checkout.91eddf5b579b45a1d1cc.js.LICENSE.txt"
+  },
+  {
+    "revision": "087528076daec42af6e8",
+    "url": "/checkout.b4fa27a1377cd52eddb0.css"
+  },
+  {
+    "revision": "caa30a3ed7d80bede772",
+    "url": "/d43e99aa2fb98108be85.js"
+  },
+  {
+    "revision": "2b100d54b696f2d34437309d3580dc20",
+    "url": "/d43e99aa2fb98108be85.js.LICENSE.txt"
+  },
+  {
+    "revision": "03e1db9ba4e1ad5982eb",
+    "url": "/da3055c92023aaf921a3.js"
+  },
+  {
+    "revision": "3714dbbb338e285d929c",
+    "url": "/dad4dad51d4461d28115.js"
+  },
+  {
+    "revision": "e5693add0d952914f2f7",
+    "url": "/dbd933c0e4260ca953ef.js"
+  },
+  {
+    "revision": "6216f19f3f5fd9895734",
+    "url": "/e6f7edc6619d18d2b259.js"
+  },
+  {
+    "revision": "1ed482cee4cb56f40f7a0c96aca59db8",
+    "url": "/e6f7edc6619d18d2b259.js.LICENSE.txt"
+  },
+  {
+    "revision": "535f4e9c20ae82fff21d",
+    "url": "/e8d107d5fc3c7a40b941.js"
+  },
+  {
+    "revision": "e5693add0d952914f2f7",
+    "url": "/eba831c4b4225958339d.css"
+  },
+  {
+    "revision": "195b36c2c364c205c090",
+    "url": "/eba91ac16d6ac18a7eb9.js"
+  },
+  {
+    "revision": "44c7254da28ca22bd127",
+    "url": "/eca0417f1a413915212f.css"
+  },
+  {
+    "revision": "195b36c2c364c205c090",
+    "url": "/f7098a8dab806e59e4bc.css"
+  },
+  {
+    "revision": "a4b1de84fd48407c66b7",
+    "url": "/f7d49b637c288a640abd.css"
+  },
+  {
+    "url": "/icon_128x128.c58f8562c132df87ecb10c0598db083c.png"
+  },
+  {
+    "url": "/icon_192x192.d3b4a59e45ac4460fb8bca55139e47f4.png"
+  },
+  {
+    "url": "/icon_384x384.0240fb21e4a86ec97b70a10a0be82bb6.png"
+  },
+  {
+    "url": "/icon_512x512.a0ba80003b3cf0f275a47885c5bf31c0.png"
+  },
+  {
+    "url": "/icon_96x96.570e1710e5824f55c3c7a5c606cbfc9c.png"
+  },
+  {
+    "revision": "e96cba55bcbc3ba6e1afdd23b7bfa4ed",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "ed6b5da0e455030b0f7b",
+    "url": "/storefront.452259759b6a9132aef3.js"
+  },
+  {
+    "revision": "9127797631718cd880aba66e21abdee0",
+    "url": "/storefront.452259759b6a9132aef3.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed6b5da0e455030b0f7b",
+    "url": "/storefront.6417719085549591f89a.css"
+  }
+]);
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.0e532720ec4a28bd056085b9f71d41e7.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.8af0cc57135cf078ccf56ce498dc8f5b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* global workbox */
 
